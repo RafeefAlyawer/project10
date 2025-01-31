cadf271dde943f0d1386394ce82e24a5af67eb70
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 15:57:36 -0500

    implemented logic to add 2 veriable

--- src/demo1/DemoTest10.java
+++ src/demo1/DemoTest10.java
@@ -3,6 +3,11 @@ package demo1;
 public class DemoTest10 {
 	 public static void main (String args []) {
 		 System.out.println("JavaIsFun");
+		 int a =10 ;
+		 int b = 20 ;
+		 int result = a - b;
+		 
+		 System.out.println(result);
 	 }
 
 }

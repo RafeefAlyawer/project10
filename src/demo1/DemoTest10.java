bbf3d8db8db596c7d9bcd4901e276d9051cd69dd
RAFEEF <RAFEEF@10.0.0.65>
2025-01-31 16:03:42 -0500

    implemented new add()

--- src/demo1/DemoTest10.java
+++ src/demo1/DemoTest10.java
@@ -2,12 +2,16 @@ package demo1;
 
 public class DemoTest10 {
 	 public static void main (String args []) {
-		 System.out.println("JavaIsFun");
+		 System.out.println("Rafeef88");
 		 int a =10 ;
 		 int b = 20 ;
 		 int result = a - b;
 		 
 		 System.out.println(result);
+	 }
+	 
+	 public void add(int a , int b) {
+		 System.out.println(a-b);
 	 }
 
 }
